b3abe2ffe5de9ba65e0d1eceb2d31507111298d0
rudnei.o <rudnei.o@samsung.com>
2020-11-24 15:53:50 -0300

    first test created

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -1,14 +1,22 @@
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { setupCourses } from '../common/setup-test-data';
 import { CoursesModule } from '../courses.module';
 import { CoursesService } from '../services/courses.service';
 import { HomeComponent } from './home.component';
+import { of } from 'rxjs';
 
 describe('HomeComponent', () => {
   let fixture: ComponentFixture<HomeComponent>;
   let component: HomeComponent;
   let el: DebugElement;
+  let coursesService: any;
+
+  const beginnerCourses = setupCourses().filter(
+    (course) => course.category === 'BEGINNER',
+  );
 
   beforeEach(
     waitForAsync(() => {
@@ -25,7 +33,22 @@ describe('HomeComponent', () => {
           fixture = TestBed.createComponent(HomeComponent);
           component = fixture.componentInstance;
           el = fixture.debugElement;
+          coursesService = TestBed.inject(CoursesService);
         });
     }),
   );
+
+  it('should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display only beginner courses', () => {
+    coursesService.findAllCourses.and.returnValue(of(beginnerCourses));
+
+    fixture.detectChanges();
+
+    const tabs = el.queryAll(By.css('.mat-tab-label'));
+
+    expect(tabs.length).toBe(1, 'Unexpected number of tabs found');
+  });
 });
